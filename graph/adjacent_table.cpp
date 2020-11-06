d40cd397634ee3288db32bbfa92816b696fd1800
nullscc <jarves@foxmail.com>
2020-11-06 17:25:01 +0800

    graph using adjacent table

---
+++ graph/adjacent_table.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+typedef struct Edge{
+    int vex;
+    Edge *next;
+}Edge, *pEdge;
+
+typedef struct Vex {
+    pEdge first;
+    char e;
+}Vex, *pVex;
+
+typedef struct {
+    pVex vexes;
+    int vnum;
+    int ednum;
+}G, *pG;
+
+pG Build(char *vexes, int vnum, std::pair<int, int> *edges, int ednum) {
+    pG pg = new G;
+    pg->vexes = new Vex[vnum];
+    pg->vnum = vnum;
+    pg->ednum = ednum;
+    
+    int i;
+    for(i=0; i<vnum; i++) {
+        pg->vexes[i].e = vexes[i];
+        pg->vexes[i].first = NULL;
+    }
+
+    for(i=0; i<ednum; i++) {
+        if(!pg->vexes[edges[i].first-1].first) {
+            pEdge pedge = new Edge;
+            pedge->vex = edges[i].second-1;
+            pedge->next = NULL;
+            pg->vexes[edges[i].first-1].first = pedge;
+        } else {
+            pEdge pedge = new Edge;
+            pedge->vex = edges[i].second-1;
+            pedge->next = pg->vexes[edges[i].first-1].first;
+            pg->vexes[edges[i].first-1].first = pedge;
+        }
+
+        if(!pg->vexes[edges[i].second-1].first) {
+            pEdge pedge = new Edge;
+            pedge->vex = edges[i].first-1;
+            pedge->next = NULL;
+            pg->vexes[edges[i].second-1].first = pedge;
+        } else {
+            pEdge pedge = new Edge;
+            pedge->vex = edges[i].first-1;
+            pedge->next = pg->vexes[edges[i].second-1].first;
+            pg->vexes[edges[i].second-1].first = pedge;
+        }
+    }
+    return pg;
+}
+
+void dfs(pG pg, int i, bool *visited, void (*visit)(pG, int)) {
+    visit(pg, i);
+    visited[i] = true;
+
+    pEdge p = pg->vexes[i].first;
+    while(p) {
+        if(!visited[p->vex]) {
+            dfs(pg, p->vex, visited, visit);
+        }
+        p = p->next;
+    }
+}
+
+void DFS(pG pg, void (*visit)(pG, int)) {
+    int i;
+    
+    bool *visited = new bool[pg->vnum];
+
+    for(i=0; i<pg->vnum; i++) {
+        visited[i] = false;
+    }
+
+    for(i=0; i<pg->vnum; i++) {
+        if(!visited[i]) {
+            dfs(pg, i, visited, visit);
+        }
+    }
+}
---
+++ graph/test_adjacent_table.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cassert>
+#include "adjacent_table.cpp"
+
+using namespace std;
+
+void visit(pG pg, int i) {
+    cout<<pg->vexes[i].e<<" ";
+}
+
+int main() {
+    // 数据使用《大话数据结构》p239(电子书p263), 图7-5-2
+    char vexes[] = "ABCDEFGHI";
+    int edges_data[] = {1,2,1,6,2,3,2,7,2,9,3,9,3,4,4,9,4,8,4,7,4,5,5,8,5,6,6,7,7,8};
+    pair<int, int> edges[15];
+
+    int i;
+    for(i=0; i<29; i+=2) {
+        pair<int, int> tmp;
+        tmp.first = edges_data[i];
+        tmp.second = edges_data[i+1];
+        edges[i/2] = tmp;
+    }
+    pG pg = Build(vexes, 9, edges, 15);
+
+    cout<<"深度优先搜索结果:";
+    DFS(pg, visit);
+    cout<<endl;
+
+    return 0;
+}
