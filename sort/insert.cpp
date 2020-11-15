946a9b44e15cf5ecaa93ea21881512bbc6f41117
nullscc <jarves@foxmail.com>
2020-11-15 15:21:57 +0800

    algorithm of straight insert

---
+++ sort/insert.cpp
@@ -0,0 +1,28 @@
+
+void insert(int *l, int n) {
+    int i, j, tmp, k;
+
+    int *ll = new int[n+1];
+    for(i=1; i<=n; i++) {
+        ll[i] = l[i-1];
+    }
+
+
+    for(i=2; i<=n; i++) {
+        if(ll[i] < ll[i-1]) {
+            ll[0] = ll[i];
+            for(j=i; ll[j-1]>ll[0]; j--) {
+                tmp = ll[j];
+                ll[j] = ll[j-1];
+                ll[j-1] = tmp;
+            }
+            ll[j] = ll[0];
+        }
+    }
+
+    for(i=0; i<n; i++) {
+        l[i] = ll[i+1];
+    }
+    
+    delete[] ll;
+}
