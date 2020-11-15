497899af81e1e77451af334b5a150f5a12e48fd2
nullscc <jarves@foxmail.com>
2020-11-15 17:25:36 +0800

    quick sort

---
+++ sort/quick.cpp
@@ -0,0 +1,31 @@
+// TODO: 优化
+
+int partition(int *l, int low, int high) {
+    int pivot_value;
+    int pivot = low;
+
+    pivot_value = l[pivot];
+    while(low<high) {
+        while(l[high] > pivot_value && low<high) high--;
+        l[pivot] = l[high];
+
+        while(l[low] < pivot_value && low<high) low++;
+        l[high] = l[low];
+    }
+    l[low] = pivot_value;
+
+    return low;
+}
+
+void quick(int *l, int low, int high) {
+    if(low >= high) return;
+    int pivot;
+    pivot = partition(l, low, high);
+
+    if(low < pivot-1) {
+        quick(l, low, pivot-1);
+    }
+    if(pivot+1 < high) {
+        quick(l, pivot+1, high);
+    }
+}
